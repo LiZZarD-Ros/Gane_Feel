27d43707b038c16d163737808ef0c71bdb43f0d8
Roscoe Sprong <140954865+LiZZarD-Ros@users.noreply.github.com>
2026-01-09 10:15:07 +0200

    First commit

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -8,10 +8,13 @@ public class Gun : MonoBehaviour
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
+
+    private Vector2 _mousePos;
     
     private void Update()
     {
         Shoot();
+        RotateGun();
     }
 
     private void Shoot()
@@ -25,4 +28,14 @@ public class Gun : MonoBehaviour
     {
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
     }
+
+    private void RotateGun()
+    {
+       _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 direction = _mousePos - (Vector2)PlayerController.Instance.transform.position;
+        Vector2 direction = PlayerController.Instance.transform.InverseTransformPoint(_mousePos);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0,0,angle);
+    }
+
 }
