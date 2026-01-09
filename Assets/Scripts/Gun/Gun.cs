0e1030891eb8efcff52f583fee2295ddce7ce91d
Roscoe Sprong <140954865+LiZZarD-Ros@users.noreply.github.com>
2026-01-09 19:37:53 +0200

    Gun Animation

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public static Action OnShoot;
+
     public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private float _gunFireCD = .5f;
+
 
     private Vector2 _mousePos;
+    private float _lastFireTime = 0f;
+
+    private Animator _animator;
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     
     private void Update()
     {
@@ -17,16 +30,36 @@ public class Gun : MonoBehaviour
         RotateGun();
     }
 
+    private void OnEnable()
+    {
+        OnShoot += ShootProjectile;
+        OnShoot += ResetLastFireTime;
+    }
+
+    private void OnDisable()
+    {
+        OnShoot -= ShootProjectile;
+        OnShoot -= ResetLastFireTime;
+    }
+
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            ShootProjectile();
+        if (Input.GetMouseButton(0) && Time.time >= _lastFireTime) {
+            OnShoot?.Invoke();
+            
         }
     }
 
     private void ShootProjectile()
     {
+        
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
+        newBullet.Init(_bulletSpawnPoint.position, _mousePos);
+    }
+
+    private void ResetLastFireTime()
+    {
+      _lastFireTime = Time.time + _gunFireCD;
     }
 
     private void RotateGun()
@@ -36,6 +69,7 @@ public class Gun : MonoBehaviour
         Vector2 direction = PlayerController.Instance.transform.InverseTransformPoint(_mousePos);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.localRotation = Quaternion.Euler(0,0,angle);
+ 
     }
 
 }
