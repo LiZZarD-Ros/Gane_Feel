cd2a92a28425f11e51b5ebf77c96a257c9d5e542
Roscoe Sprong <140954865+LiZZarD-Ros@users.noreply.github.com>
2026-02-04 20:27:54 +0200

    Combat Finished

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,9 +1,16 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Health : MonoBehaviour
 {
+    public GameObject SplatterPrefab => _splatterPrefab;
+    public GameObject DeathVFX => _deathVFX;
+    public static Action<Health> OnDeath;
+
+    [SerializeField] private GameObject _splatterPrefab;
+    [SerializeField] private GameObject _deathVFX;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
@@ -11,6 +18,7 @@ public class Health : MonoBehaviour
     private void Start() {
         ResetHealth();
     }
+
 
     public void ResetHealth() {
         _currentHealth = _startingHealth;
@@ -20,7 +28,10 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
+            OnDeath?.Invoke(this);
             Destroy(gameObject);
+
         }
     }
+
 }
