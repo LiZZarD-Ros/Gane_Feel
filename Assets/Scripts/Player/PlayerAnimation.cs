4cadbd938a859f1cbc09a99b336990db9e2d4c03
Roscoe Sprong <140954865+LiZZarD-Ros@users.noreply.github.com>
2026-02-06 15:40:29 +0200

    VFX done

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,19 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _moveDustVFX;
+    [SerializeField] private ParticleSystem _poofDustVFX;
+
     [SerializeField] private float _tiltAngle = 20f;
     [SerializeField] private float _tiltSpeed = 5f;
+
     [SerializeField] private Transform _characterSpriteTransform;
+    [SerializeField] private Transform _cowboyHatTransform;
+    [SerializeField] private float _cowboyHatTiltModifier = 2f;
+
+    [SerializeField] private float _yLandVelocityCheck = -10f;
+    private Vector2 _velocityBeforePhysiscsUpdate;
+    private Rigidbody2D _rigidBody;
+    private CinemachineImpulseSource _impulseSource;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+
+    }
 
     private void Update()
     {
         DetectMoveDust();
         ApplyTilt();
     }
+
+    private void OnEnable()
+    {
+        PlayerController.OnJump += PlayPoofDustVFX;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.OnJump -= PlayPoofDustVFX;
+    }
+
+
+    private void FixedUpdate()
+    {
+        _velocityBeforePhysiscsUpdate = _rigidBody.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (_velocityBeforePhysiscsUpdate.y < _yLandVelocityCheck)
+        {
+            PlayPoofDustVFX();
+            _impulseSource.GenerateImpulse();
+        }
+    }
+
 
     private void DetectMoveDust()
     {
@@ -32,6 +76,11 @@ public class PlayerAnimation : MonoBehaviour
         }
     }
 
+    private void PlayPoofDustVFX()
+    {
+        _poofDustVFX.Play();
+    }
+
     private void ApplyTilt()
     {
         float targetAngle;
@@ -47,10 +96,17 @@ public class PlayerAnimation : MonoBehaviour
             targetAngle = 0f;
         }
 
+        // Player Sprite Tilt
         Quaternion currentCharacterRotation = _characterSpriteTransform.rotation;
         Quaternion targetCharacterRotation = Quaternion.Euler(currentCharacterRotation.eulerAngles.x, currentCharacterRotation.eulerAngles.y, targetAngle);
 
         _characterSpriteTransform.rotation = Quaternion.Lerp(currentCharacterRotation, targetCharacterRotation, _tiltSpeed * Time.deltaTime);
+
+        // Cowboy Hat Sprite
+        Quaternion currentHatRotation = _cowboyHatTransform.rotation;
+        Quaternion targetHatRotation = Quaternion.Euler(currentHatRotation.eulerAngles.x, currentHatRotation.eulerAngles.y, -targetAngle / _cowboyHatTiltModifier);
+
+        _cowboyHatTransform.rotation = Quaternion.Lerp(currentHatRotation, targetHatRotation, _tiltSpeed * Time.deltaTime * _cowboyHatTiltModifier);
 
     }
 
