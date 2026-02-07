264b70d9b9a522e0c3d4f7702e3c61e82591ff31
Roscoe Sprong <140954865+LiZZarD-Ros@users.noreply.github.com>
2026-02-07 20:26:46 +0200

    Sound Manager
    Sound

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private float _masterVolume = 1f;
+    [SerializeField] private SoundSO _gunShoot;
+    [SerializeField] private SoundSO _jump;
+
+    private void OnEnable()
+    {
+        Gun.OnShoot += Gun_OnShoot;
+        PlayerController.OnJump += PalayerController_OnJump;
+    }
+
+
+    private void OnDisable()
+    {
+        Gun.OnShoot -= Gun_OnShoot;
+        PlayerController.OnJump -= PalayerController_OnJump;
+    }
+
+    private void SoundToPlay(SoundSO soundSO)
+    {
+        AudioClip clip = soundSO.Clip;
+        float pitch = soundSO.Pitch;
+        float volume = soundSO.Volume * _masterVolume;
+        bool loop = soundSO.Loop;
+
+        if (soundSO.RandomizePitch)
+        {
+            float randomPitchModifier = Random.Range(-soundSO.RandomPitchRangeModifier, soundSO.RandomPitchRangeModifier);
+            pitch = soundSO.Pitch + randomPitchModifier;
+        }
+
+        PlaySound(clip, pitch, volume, loop);
+    }
+
+    private void PlaySound(AudioClip clip, float pitch, float volume, bool loop)
+    {
+        GameObject soundObject = new GameObject("Temp Audio Source");
+        AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.pitch = pitch;
+        audioSource.volume = volume;
+        audioSource.loop = loop;
+        audioSource.Play();
+
+        if (!loop) { Destroy(soundObject, clip.length); }
+    }
+    
+    
+    private void Gun_OnShoot()
+    {
+        SoundToPlay(_gunShoot);
+    }
+
+    private void PalayerController_OnJump()
+    {
+        SoundToPlay(_jump);
+    }
+
+
+}
